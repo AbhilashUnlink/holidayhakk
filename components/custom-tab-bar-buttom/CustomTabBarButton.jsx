3cee08c5b726c2efe4183916f7eff4b8069c9812
AbhilashUnlink <abhilash.sharma@unlink-technologies.com>
2023-05-05 19:59:09 +0530

    nothing

--- components/custom-tab-bar-buttom/CustomTabBarButton.jsx
+++ components/custom-tab-bar-buttom/CustomTabBarButton.jsx
@@ -7,9 +7,9 @@ const CustomTabBarButton = (props) => {
   const { route, children, accessibilityState, onPress } = props;
 
   if (accessibilityState.selected) {
-    
     return (
       <View style={styles.btnWrapper}>
+        <Text>hello</Text>
         <View style={{ flexDirection: "row" }}>
           <View
             style={[
@@ -39,7 +39,6 @@ const CustomTabBarButton = (props) => {
           activeOpacity={1}
           onPress={onPress}
           style={[styles.activeBtn]}
-
         >
           <View style={styles.button}>
             <Text>{children}</Text>
